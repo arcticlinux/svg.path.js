de3d511cb6e0e70dead383488a51aba8387b65ce
任羽飞 <ren12345@126.com>
2017-03-21 10:03:03 +0800

    remove duplicate code

--- svg.path.js
+++ svg.path.js
@@ -18,59 +18,27 @@
 
 			return this;
 		},
-		m: function(p){
-			p = (arguments.length === 1) ? [p.x, p.y] : slice(arguments);
-
-			this.addSegment('m', p, this._redrawEnabled);
-
-			if (this._segments.length === 1){
-				return this.plot('m' + p[0] + ' ' + p[1]);
-			}
-
-			return this;
-		},
 		// TODO: Solve
 		L: function(p) {
 			p = (arguments.length === 1) ? [p.x, p.y] : slice(arguments);
 
 			return this.addSegment('L', p, this._redrawEnabled);
 		},
-		l: function(p) {
-			p = (arguments.length === 1) ? [p.x, p.y] : slice(arguments);
-
-			return this.addSegment('l', p, this._redrawEnabled);
-		},
 		H: function(x){
 			return this.addSegment('H', [x], this._redrawEnabled);
 		},
-		h: function(x){
-			return this.addSegment('h', [x], this._redrawEnabled);
-		},
 		V: function(y){
 			return this.addSegment('V', [y], this._redrawEnabled);
-		},
-		v: function(y){
-			return this.addSegment('v', [y], this._redrawEnabled);
 		},
 		C: function(p1, p2, p){
 			p = (arguments.length === 3) ? [p1.x, p1.y, p2.x, p2.y, p.x, p.y] : slice(arguments);
 
 			return this.addSegment('C', p, this._redrawEnabled);
 		},
-		c: function(p1, p2, p){
-			p = (arguments.length === 3) ? [p1.x, p1.y, p2.x, p2.y, p.x, p.y] : slice(arguments);
-
-			return this.addSegment('c', p, this._redrawEnabled);
-		},
 		S: function(p2, p){
 			p = (arguments.length === 2) ? [p2.x, p2.y, p.x, p.y] : slice(arguments);
 
 			return this.addSegment('S', p, this._redrawEnabled);
-		},
-		s: function(p2, p){
-			p = (arguments.length === 2) ? [p2.x, p2.y, p.x, p.y] : slice(arguments);
-
-			return this.addSegment('s', p, this._redrawEnabled);
 		},
 		// Q x1 y1, x y
 		Q: function(p1, p){
@@ -78,30 +46,15 @@
 
 			return this.addSegment('Q', p, this._redrawEnabled);
 		},
-		q: function(p1, p){
-			p = (arguments.length === 2) ? [p1.x, p1.y, p.x, p.y] : slice(arguments);
-
-			return this.addSegment('q', p, this._redrawEnabled);
-		},
 		T: function(p){
 			p = (arguments.length === 1) ? [p.x, p.y] : slice(arguments);
 
 			return this.addSegment('T', p, this._redrawEnabled);
 		},
-		t: function(p){
-			p = (arguments.length === 1) ? [p.x, p.y] : slice(arguments);
-
-			return this.addSegment('t', p, this._redrawEnabled);
-		},
 		A: function(rx, ry, xAxisRotation, largeArcFlag, sweepFlag, p){
 			p = (arguments.length === 6) ? [rx, ry, xAxisRotation, largeArcFlag, sweepFlag, p.x, p.y] : slice(arguments);
 
 			return this.addSegment('A', p, this._redrawEnabled);
-		},
-		a: function(rx, ry, xAxisRotation, largeArcFlag, sweepFlag, p){
-			p = (arguments.length === 6) ? [rx, ry, xAxisRotation, largeArcFlag, sweepFlag, p.x, p.y] : slice(arguments);
-
-			return this.addSegment('a', p, this._redrawEnabled);
 		},
 		Z: function(){
 			return this.addSegment('Z', [], this._redrawEnabled);
